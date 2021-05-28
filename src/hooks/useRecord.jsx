1cbbb62f141e2bfdff15667f7a3e9d60f5246247
Devon Wolfkiel <devonwolf@tutanota.com>
2021-05-28 11:02:46 -0700

    Set up initial undo and redo functionality

--- src/hooks/useRecord.jsx
+++ src/hooks/useRecord.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const useRecord = (init) => {
   const [current, setCurrent] = useState(init);
-  const record = (newValue) => setCurrent(newValue);
-	
+  const [recordArray, setRecordArray] = useState([init]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    setCurrentIndex(recordArray.indexOf(current));
+  }, [current]);
+  
+  const record = (newValue) => {
+    setRecordArray(prevArray => [...prevArray, newValue]);    
+    setCurrent(newValue);
+  };
+
+  const undo = () => {
+    setCurrent(recordArray[currentIndex - 1]);
+  };
+
+  const redo = () => {
+    setCurrent(recordArray[currentIndex + 1]);
+  };
+  
+  console.log(recordArray);
+
   return {
     current,
-    undo: () => {},
-    redo: () => {},
+    undo,
+    redo,
     record
   };
 };
