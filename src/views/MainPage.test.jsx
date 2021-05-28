4a3d7e61cddfeff5c34e1b5f78235bb48f5f3ba2
Devon Wolfkiel <devonwolf@tutanota.com>
2021-05-28 15:22:14 -0700

    Fix issues with testing, fill out behavioral testing

--- src/views/MainPage.test.jsx
+++ src/views/MainPage.test.jsx
@@ -9,37 +9,82 @@ describe('Color picker', () => {
   it('renders a correctly functioning color picker', () => {
     render(<MainPage />);
 
+    const colorPicker = screen.getByLabelText('color-picker');
+    const colorDisplay = screen.getByLabelText('color-display');
+
+    fireEvent.change(colorPicker, { target: { value: '#6BD425' } });
+    fireEvent.change(colorPicker, { target: { value: '#618B25' } });
+    fireEvent.change(colorPicker, { target: { value: '#42113C' } });
+    fireEvent.change(colorPicker, { target: { value: '#370926' } });
+    
+    return waitFor(() => {
+      expect(colorDisplay).toHaveStyle({ 'background-color': '#370926' });
+    });
+  });
+
+  it('goes backward on undo', () => {
+    render(<MainPage />);
+
+    const undoButton = screen.getByLabelText('undo');
+    const colorPicker = screen.getByLabelText('color-picker');
+    const colorDisplay = screen.getByLabelText('color-display');
+
+    fireEvent.change(colorPicker, { target: { value: '#6BD425' } });
+    fireEvent.change(colorPicker, { target: { value: '#618B25' } });
+    fireEvent.change(colorPicker, { target: { value: '#42113C' } });
+
+    fireEvent.click(undoButton);
+
+    return waitFor(() => {
+      expect(colorDisplay).toHaveStyle({ 'background-color': '#618B25' });
+    });
+  });
+
+  it('goes forward on redo', () => {
+    render(<MainPage />);
+    
     const undoButton = screen.getByLabelText('undo');
     const redoButton = screen.getByLabelText('redo');
     const colorPicker = screen.getByLabelText('color-picker');
     const colorDisplay = screen.getByLabelText('color-display');
 
-    // these tests are not working right, passing when they shouldn't be - see first event fire
-    fireEvent.input(colorPicker, { target: { value: '#6BD425' } });
-    waitFor(() => {
-      expect(colorDisplay).toHaveStyle({ backgroundColor: 'hot-pink' });
+    fireEvent.change(colorPicker, { target: { value: '#6BD425' } });
+    fireEvent.change(colorPicker, { target: { value: '#618B25' } });
+    fireEvent.change(colorPicker, { target: { value: '#42113C' } });
+
+    fireEvent.click(undoButton);
+    fireEvent.click(undoButton);
+    fireEvent.click(redoButton);
+
+    return waitFor(() => {
+      expect(colorDisplay).toHaveStyle({ 'background-color': '#618B25' });
     });
+  });
 
-    // fireEvent.input(colorPicker, '#618B25');
-    // waitFor(() => {
-    //   expect(colorDisplay).toHaveStyle({ 'backgroundColor': '#618B25' });
-    // });
+  it('inserts a new color at the current position of the history/record', () => {
+    render(<MainPage />);
+    
+    const undoButton = screen.getByLabelText('undo');
+    const redoButton = screen.getByLabelText('redo');
+    const colorPicker = screen.getByLabelText('color-picker');
+    const colorDisplay = screen.getByLabelText('color-display');
 
-    // fireEvent.input(colorPicker, '#42113C');
-    // waitFor(() => {
-    //   expect(colorDisplay).toHaveStyle({ 'backgroundColor': '#42113C' });
-    // });
+    fireEvent.change(colorPicker, { target: { value: '#6BD425' } });
+    fireEvent.change(colorPicker, { target: { value: '#618B25' } });
+    fireEvent.change(colorPicker, { target: { value: '#42113C' } });
 
-    // fireEvent.input(colorPicker, '#370926');
-    // waitFor(() => {
-    //   expect(colorDisplay).toHaveStyle({ 'backgroundColor': '#370926' });
-    // });
+    fireEvent.click(undoButton);
+    fireEvent.click(undoButton);
 
-    // fireEvent.input(colorPicker, '#1C0118');
-    // waitFor(() => {
-    //   expect(colorDisplay).toHaveStyle({ 'backgroundColor': '#6BD425' });
-    // });
+    fireEvent.change(colorPicker, { target: { value: '#FFFFFF ' } });
+    
+    fireEvent.click(undoButton);
+    fireEvent.click(redoButton);
+    fireEvent.click(redoButton);
 
+    return waitFor(() => {
+      expect(colorDisplay).toHaveStyle({ 'background-color': '#618B25' });
+    });
 
   });
 });
